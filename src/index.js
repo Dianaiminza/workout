ac4440bf8833ac0340a6517b2d2f039e0de05b8a
diana.iminza <diana.iminza@ronforddigital.com>
2022-05-06 14:34:31 +0300

    my first commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const app = express(); 
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const PORT = process.env.PORT || 3000; 
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
+    V1SwaggerDocs(app, PORT);
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const workoutController = require("../../controllers/workoutController");
-
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ */
 router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
